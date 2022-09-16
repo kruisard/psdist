fbecb6a19e2388a5ecbef976f7847f8f9ba186f3
austin-hoover <hooveram@ornl.gov>
2022-09-16 13:03:44 -0400

    Add get_bin_centers and ndhistogram with auto bins

--- utils.py
+++ utils.py
@@ -105,6 +105,17 @@ def get_grid_coords(*xi, indexing='ij'):
     return np.vstack([X.ravel() for X in np.meshgrid(*xi, indexing=indexing)]).T
 
 
+def get_bin_centers(edges):
+    """Compute bin centers from bin edges."""
+    return 0.5 * (edges[:-1] + edges[1:])
+
+
+def histogramdd(a, bins='auto'):
+    """Multi-dimensional histogram with auto bins along each axis."""
+    edges = [np.histogram_bin_edges(a[:, j], bins) for j in range(a.shape[1])]
+    return np.histogramdd(a, edges)
+    
+
 # The following three functions are from Tony Yu's blog 
 # (https://tonysyu.github.io/ragged-arrays.html#.YKVwQy9h3OR).
 def stack_ragged(array_list, axis=0):
