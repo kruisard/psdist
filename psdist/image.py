fee3c5cd4c924054afac9cee3839493e2d4f6209
Hoover, Austin <hooveram@ornl.gov>
2022-10-11 07:37:57 -0400

    Add radial density computation; rename project1d_contour

--- psdist/image.py
+++ psdist/image.py
@@ -70,3 +70,63 @@ def project(image, axis=0):
             proj = np.swapaxes(proj, i, j)
             loc[i], loc[j] = loc[j], loc[i]
     return proj
+
+
+def project1d_contour(f, axis=0, level=0.1, fpr=None, normalize=True, return_frac=False):
+    axis_proj = [i for i in range(f.ndim) if i != axis]
+    if fpr is None:
+        fpr = project(f, axis_proj)
+    fpr = fpr / np.max(fpr)
+    idx = np.where(fpr > level)
+    frac = np.sum(fpr[idx]) / np.sum(fpr)
+    idx = make_slice(f.ndim, axis_proj, idx)    
+    p = np.sum(f[idx], axis=int(axis == 0))
+    if normalize:
+        p = p / np.sum(p)
+    if return_frac:
+        return p, frac
+    return p
+
+
+def project2d_contour(f, level=0.1, fpr=None, normalize=True, return_frac=False, axis=(2, 3)):
+    # Compute the 3D mask.
+    axis_proj = [i for i in range(f.ndim) if i not in axis]
+    if fpr is None:
+        fpr = utils.project(f, axis_proj)
+    fpr = fpr / np.max(fpr)
+    mask = fpr < level
+    frac = np.sum(fpr[~mask]) / np.sum(fpr)
+
+    # Copy the 3D mask into the two projected dimensions.
+    mask = utils.copy_into_new_dim(mask, (f.shape[axis[0]], f.shape[axis[1]]), axis=-1, copy=True)
+    # Put the dimensions in the correct order.        
+    isort = np.argsort(list(axis_proj) + list(axis))
+    mask = np.moveaxis(mask, isort, np.arange(5))
+    
+    # Project the masked 5D array onto the specified axis.    
+    im = utils.project(np.ma.masked_array(f, mask=mask), axis=axis)
+    if return_frac:
+        return im, frac
+    return im
+
+
+def get_radii(coords, Sigma):
+    COORDS = np.meshgrid(*coords, indexing='ij')
+    shape = tuple([len(c) for c in coords])
+    R = np.zeros(shape)
+    Sigma_inv = np.linalg.inv(Sigma)
+    for ii in tqdm(np.ndindex(shape)):
+        vec = np.array([C[ii] for C in COORDS])
+        R[ii] = np.sqrt(np.linalg.multi_dot([vec.T, Sigma_inv, vec]))
+    return R
+
+
+def radial_density(f, R, radii, dr=None):
+    if dr is None:
+        dr = 0.5 * np.max(R) / (len(R) - 1)
+    fr = []
+    for r in tqdm(radii):
+        f_masked = np.ma.masked_where(np.logical_or(R < r, R > r + dr), f)
+        # mean density within this shell...
+        fr.append(np.mean(f_masked))
+    return np.array(fr)
